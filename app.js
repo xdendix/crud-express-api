d70951a0b0c5a8f90620fffe3e454da647e378e5
Dendi Prayogo Hidayat <dendiprayogo24@gmail.com>
2022-10-09 11:12:16 +0700

    Adding dataproducts router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const datausersRouter = require('./routes/datausers');
+const dataproductsRouter = require('./routes/dataproducts');
 
 const app = express();
 
@@ -19,5 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/datausers', datausersRouter);
+app.use('/dataproducts', dataproductsRouter);
 
 module.exports = app;
